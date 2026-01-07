7024596f2b6a5670b322d2a7eae2b812522cd67a
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-01-07 20:07:47 +0500

    add meeting ending button on history page

--- frontend/src/pages/history.jsx
+++ frontend/src/pages/history.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { Card, CardContent, Typography, IconButton, Box } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
+import server from "../environment";
 
 export default function History() {
   const { getHistoryOfUser } = useContext(AuthContext);
@@ -25,6 +26,34 @@ export default function History() {
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleDateString("en-GB");
+  };
+
+  const hanldeMeetingEnd = async (e) => {
+    try {
+      const res = await fetch(`${server}/api/v1/meetings/cancel`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ meetingId: e.meetingCode }),
+      });
+      const data = await res.json();
+      if (data.status === 200) {
+        alert("Meeting Ended Successfully");
+        const updatedMeetings = meetings.map((meeting) => {
+          if (meeting.meetingCode === e.meetingCode) {
+            return { ...meeting, IsLiveNow: false };
+          }
+          return meeting;
+        });
+        setMeetings(updatedMeetings);
+      } else {
+        alert("Error ending meeting");
+      }
+      // console.log(e);
+    } catch (error) {
+      throw new Error("Error ending meeting");
+    }
   };
 
   return (
@@ -92,9 +121,26 @@ export default function History() {
                   Date: {formatDate(e.date)}
                 </Typography>
               </CardContent>
-              <div>
+              <div className="text-center">
                 Meeting Is {e.IsLiveNow ? "Live" : "Ended"} <br />
-              <button>Delete</button>
+                <div className="flex my-3 gap-4 items-center">
+                  {e.IsLiveNow && (
+                    <button
+                      className="py-1 px-4 rounded-xl bg-blue-600"
+                      onClick={() => routeTo(`/${e.meetingCode}`)}
+                    >
+                      Join
+                    </button>
+                  )}
+                  {e.IsLiveNow && (
+                    <button
+                      onClick={() => hanldeMeetingEnd(e)}
+                      className="py-1 px-4 rounded-xl bg-red-600"
+                    >
+                      End Metting
+                    </button>
+                  )}
+                </div>
               </div>
             </Card>
           ))}
